9b1c6ec687a8a7b4f9efe0a5cc09b5a808fc5723
Michael Shepherd M <shepherd.munemo@outlook.com>
2023-09-13 15:01:46 +0200

    project set up

---
+++ CloudCustomers.API/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudCustomers.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ILogger<UsersController> _logger;
+        public UsersController(ILogger<UsersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetUsers")]
+        public async Task<IActionResult> Get()
+        => null;
+    }
+}
