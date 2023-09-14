cbca6fabefec2ed2365358aa37f8b8abba24ebab
Michael Shepherd M <shepherd.munemo@outlook.com>
2023-09-14 15:42:41 +0200

    two failed tests need to be fixed

--- CloudCustomers.API/Program.cs
+++ CloudCustomers.API/Program.cs
@@ -1,3 +1,4 @@
+using CloudCustomers.API.Config;
 using CloudCustomers.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -28,6 +29,7 @@ app.Run();
 
 void ConfigureServices(IServiceCollection services)
 {
+    services.Configure<UsersApiOptions>(builder.Configuration.GetSection("UsersApiOptions"));
     services.AddTransient<IUsersService, UsersService>();
     services.AddHttpClient<IUsersService, UsersService>();
 }
